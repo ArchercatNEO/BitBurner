69d0b1e488dc3929ae3c1ea197beb54a5ae50094
ArchercatNEO <tururu.pompella@gmail.com>
2024-03-07 17:30:24 +0000

    Refactored gang.ts to appease typescript

--- src/bn/gang.ts
+++ src/bn/gang.ts
@@ -1,4 +1,4 @@
-import { GangMemberInfo, GangTaskStats, NS } from "@ns";
+import { GangGenInfo, GangMemberInfo, GangTaskStats, NS } from "@ns";
 
 export async function main(ns: NS) {
     //shortcut
@@ -8,7 +8,6 @@ export async function main(ns: NS) {
 
     const warfareStatReq = 1000; //strength to send someone to war
     const warfareMemReq = 11; //cult members required to prep for war - 1
-    const clashChanceReq = 0.55; //chance we wait for to declare war
 
     const wantedThresh = 0.5; //wanted nerf to become batman
     const penaltyStatReq = 200; //strength to become batman
@@ -36,7 +35,7 @@ export async function main(ns: NS) {
         "Hydroflame"
     ];
     //Fill up the last of the name list
-    for (let stamp = gangNames.length; stamp < 12; stamp++) gangNames.push(`Lakey #${stamp}`);
+    for (let stamp = gangNames.length; stamp < 12; stamp++) { gangNames.push(`Lakey #${stamp}`); }
 
     //set up constant lists of equipment to buy later
     const everything = gang.getEquipmentNames();
@@ -47,68 +46,84 @@ export async function main(ns: NS) {
 
     // eslint-disable-next-line no-constant-condition
     while (true) {
-        //fill up on lakeys
+        const members = gang.getMemberNames();
+        const snake = gang.getGangInformation();
+        
+        //Fill up on members
         while (gang.canRecruitMember()) {
             //get the first name in the list we haven't used already
-            const name = gangNames.filter((a) => !gang.getMemberNames().includes(a))[0];
+            const name = gangNames.filter((a) => !members.includes(a))[0];
             gang.recruitMember(name);
             gang.setMemberTask(name, "Train Combat");
+            members.push(name);
         }
 
-        const members = gang.getMemberNames();
-
-        //reset gang information
-        const snake = gang.getGangInformation();
-        let goal: (task, member) => unknown;
-
-        //ascend everyone
+        //Ascend everyone
         for (const name of members) {
-            if (
-                gang.getAscensionResult(name) != undefined &&
-                gang.getAscensionResult(name).str > calculateAscendTreshold(name) &&
-                gang.getMemberInformation(name).task != "Territory Warfare"
-            )
-                gang.ascendMember(name);
+            const result = gang.getAscensionResult(name);
+            if (result === undefined) { return; }
+            if (result.str > calculateAscendTreshold(name)) { return; }
+            if (gang.getMemberInformation(name).task != "Territory Warfare") { return; }
+            gang.ascendMember(name);
         }
-        //I need smarts to set goal smartly
-        //gang warfare
-        if (members.length > warfareMemReq && snake.territory < 1) {
-            goal = warfare;
-            //if (gang.getChanceToWinClash() > clashChanceReq)
-            //gang.setTerritoryWarfare(true)
-        } else if (snake.wantedPenalty < wantedThresh) goal = penalty;
-        else if (lackMoney() || (snake.respect > respectThresh && snake.territory == 1))
-            goal = money;
-        else goal = respect;
 
-        for (const name of members) {
-            if (gang.getAscensionResult(name) > calculateAscendTreshold(name))
-                gang.ascendMember(name);
-        }
-        //buy the same equip for all lakeys then move on to the next eqip
-        for (const equip of equipment)
+        //Buy the same item across every member to spend evenly
+        for (const equip of equipment) {
             for (const name of members) {
-                const member = gang.getMemberInformation(name);
-
                 //buy equipment that costs less than budget% of our money
-                if (gang.getEquipmentCost(equip) < ns.getPlayer().money * budget)
+                if (gang.getEquipmentCost(equip) < ns.getPlayer().money * budget) {
                     gang.purchaseEquipment(name, equip);
-
-                //set tasks
-                const bestTask = plans.reduce(
-                    (a, b) => (goal(a, member) > goal(b, member) ? a : b),
-                    "Train Combat"
-                );
-                gang.setMemberTask(name, bestTask);
+                }
             }
+        }
+
+        const needsMoney = !ns.isRunning("batcher.js") && !ns.isRunning("corp.js");
+        for (const name in members) {
+            const member = gang.getMemberInformation(name);
+            
+            const goal: (task: string) => number = (task) => {
+                const stats = ns.gang.getTaskStats(task);
+                
+                //If the member is not ready do not use them
+                if (task == "Train Combat" && member.str < trainLimit) {return 1;}
+                
+                //Warfare
+                //TODO Activate territory warfare on some condition
+                if (members.length > warfareMemReq && snake.territory < 1) {
+                    if (member.str > warfareStatReq && task === "Territory Warfare") { return 1; }
+                    return respectGain(snake, member, stats);
+                } 
+                
+                //Penalty, we check for training because other tasks raise penalty
+                //TODO use formulas to check penalty drop/gain
+                if (snake.wantedPenalty < wantedThresh) {
+                    if (task == "Vigilante Justice" && member.str > penaltyStatReq) {return 1;}
+                    if (task == "Train Combat") {return 1;}
+                    return 0;
+                }  
+                
+                //Money
+                if (needsMoney || (snake.respect > respectThresh)) {
+                    return moneyGain(snake, member, stats)
+                }  
+
+                //Respect
+                return respectGain(snake, member, stats);
+            };
+    
+            //set tasks
+            const bestTask = plans.reduce(
+                (current, next) => (goal(current) > goal(next) ? current : next),
+                "Train Combat"
+            );
+            gang.setMemberTask(name, bestTask);
+        }
 
         await ns.sleep(spacer);
     }
 
-    function lackMoney() {
-        return !ns.isRunning("batcher.js") || !ns.isRunning("corp.js");
-    }
-    function calculateAscendTreshold(member: string) {
+    /** Used for deciding if an ascension is worthwhile */
+    function calculateAscendTreshold(member: string): number {
         const mult = ns.gang.getMemberInformation(member).str_asc_mult;
         if (mult < 1.632) return 1.6326;
         if (mult < 2.336) return 1.4315;
@@ -127,58 +142,14 @@ export async function main(ns: NS) {
         return 1.04;
     }
 
-    function penalty(task: string, member: GangMemberInfo) {
-        if (task == "Train Combat" && member.str < penaltyStatReq) return 1;
-        if (task == "Vigilante Justice" && member.str > penaltyStatReq) return 1;
-        return 0;
-    }
-
-    function warfare(task: string, member: GangMemberInfo) {
-        if (member.str < warfareStatReq) return respect(task, member);
-        if (task == "Territory Warfare") return 1;
-        return 0;
-    }
-
-    //pull from formulas.exe source code
-    function respect(task: string, member: GangMemberInfo) {
-        if (member.str < trainLimit) {
-            if (task == "Train Combat") return 1;
-            return 0;
+    /** Wrapper around ns.formulas with hardcoded logic as a backup */
+    function moneyGain(gang: GangGenInfo, member: GangMemberInfo, task: GangTaskStats): number {
+        if (ns.fileExists("Formulas.exe")) {
+            return ns.formulas.gang.moneyGain(gang, member, task)
         }
 
-        const stats = gang.getTaskStats(task);
-        if (stats.baseRespect === 0 || stats.territory == undefined) return 0;
-        let statWeight =
-            (stats.hackWeight / 100) * member.hack +
-            (stats.strWeight / 100) * member.str +
-            (stats.defWeight / 100) * member.def +
-            (stats.dexWeight / 100) * member.dex +
-            (stats.agiWeight / 100) * member.agi +
-            (stats.chaWeight / 100) * member.cha;
-        statWeight -= 4 * stats.difficulty;
-        if (statWeight <= 0) return 0;
-        const territoryMult = Math.max(
-            0.005,
-            Math.pow(snake.territory * 100, stats.territory.respect) / 100
-        );
-        const territoryPenalty = 0.2 * snake.territory + 0.8;
-        if (isNaN(territoryMult) || territoryMult <= 0) return 0;
-        const respectMult = snake.respect / (snake.respect + snake.wantedLevel);
-        const value = Math.pow(
-            11 * stats.baseRespect * statWeight * territoryMult * respectMult,
-            territoryPenalty
-        );
-        return value;
-    }
-
-    function money(task, member) {
-        if (member.str < trainLimit) {
-            if (task == "Train Combat") return 1;
-            return 0;
-        }
-        task = gang.getTaskStats(task);
         if (task.baseMoney === 0 || task.territory == undefined) return 0;
-        const statWeight =
+        let statWeight =
             (task.hackWeight / 100) * member.hack +
             (task.strWeight / 100) * member.str +
             (task.defWeight / 100) * member.def +
@@ -189,14 +160,44 @@ export async function main(ns: NS) {
         if (statWeight <= 0) return 0;
         const territoryMult = Math.max(
             0.005,
-            Math.pow(snake.territory * 100, task.territory.money) / 100
+            Math.pow(gang.territory * 100, task.territory.money) / 100
         );
-        const territoryPenalty = 0.2 * snake.territory + 0.8;
+        const territoryPenalty = 0.2 * gang.territory + 0.8;
         if (isNaN(territoryMult) || territoryMult <= 0) return 0;
-        const respectMult = snake.respect / (snake.respect + snake.wantedLevel);
+        const respectMult = gang.respect / (gang.respect + gang.wantedLevel);
         return Math.pow(
             11 * task.baseMoney * statWeight * territoryMult * respectMult,
             territoryPenalty
         );
+    }
+
+    /** Wrapper around ns.formulas with hardcoded logic as a backup */
+    function respectGain(gang: GangGenInfo, member: GangMemberInfo, task: GangTaskStats): number {
+        if (ns.fileExists("Formulas.exe")) {
+            return ns.formulas.gang.respectGain(gang, member, task);
+        }
+
+        if (task.baseRespect === 0 || task.territory == undefined) return 0;
+        let statWeight =
+            (task.hackWeight / 100) * member.hack +
+            (task.strWeight / 100) * member.str +
+            (task.defWeight / 100) * member.def +
+            (task.dexWeight / 100) * member.dex +
+            (task.agiWeight / 100) * member.agi +
+            (task.chaWeight / 100) * member.cha;
+        statWeight -= 4 * task.difficulty;
+        if (statWeight <= 0) return 0;
+        const territoryMult = Math.max(
+            0.005,
+            Math.pow(gang.territory * 100, task.territory.respect) / 100
+        );
+        const territoryPenalty = 0.2 * gang.territory + 0.8;
+        if (isNaN(territoryMult) || territoryMult <= 0) return 0;
+        const respectMult = gang.respect / (gang.respect + gang.wantedLevel);
+        const value = Math.pow(
+            11 * task.baseRespect * statWeight * territoryMult * respectMult,
+            territoryPenalty
+        );
+        return value;
     }
 }
